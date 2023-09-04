22dbc31787737ecff479b34e06553962ce1fdadc
Koushik Saha <www.koushiksaha.master@gmail.com>
2023-09-04 16:50:52 +0600

    new app

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Juicy Grapes',
+            desc: '',
+            price: 46,
+        },
+
+        {
+            id: 2,
+            img: img2,
+            title: 'Juicy Grapes',
+            desc: '',
+            price: 46,
+        },
+
+        {
+            id: 3,
+            img: img3,
+            title: 'Juicy Grapes',
+            desc: '',
+            price: 46,
+        },
+
+        {
+            id: 4,
+            img: img4,
+            title: 'Juicy Grapes',
+            desc: '',
+            price: 46,
+        }
+    ],
+};
+
+export default data;
